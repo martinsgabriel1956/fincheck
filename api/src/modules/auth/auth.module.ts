1474d71404ccd603eb1f78da95c51d128eff4c41
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-06-28 20:03:35 -0300

    feat: create signin, signup and me endpoint and implement authentication using jwt

---
+++ api/src/modules/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { env } from 'src/shared/config/env';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      signOptions: { expiresIn: '7d' },
+      secret: env.jwtSecret,
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
--- api/src/modules/users/users.controller.ts
+++ api/src/modules/users/users.controller.ts
@@ -1,19 +1,13 @@
-import { Controller, Post, Body, Patch, Param } from '@nestjs/common';
+import { Controller, Get, Request } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  @Get('/me')
+  me(@Request() request: any) {
+    console.log({ myUserId: request.userId });
+    return this.usersService.getUserById('userId');
   }
 }
